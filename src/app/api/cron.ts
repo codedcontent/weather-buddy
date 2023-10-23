6c335c130d57ad9e8d715d29a68ec8f7cefc2579
unknown <ogemephors002@gmail.com>
2023-10-23 12:01:50 +0100

    Erasing access full access to app features for mvp

--- src/app/api/cron.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export default function handler(req: Request) {
-  console.log("Cron Call");
-  //   res.status(200).end("Hello Cron!");
-  return NextResponse.json(JSON.stringify({ msg: "Hey" }), {
-    status: 200,
-  });
-}
